6ca5f41b92030044919a4a928cc12fea463fd709
Jisan Jung <jjjung13@gmail.com>
2019-06-22 15:07:53 -0400

    comments and organization

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-var add = document.getElementById("add-btn");
-var input = document.getElementById("user-input");
+var add = document.getElementById("add-btn");// button to add to the list
+var input = document.getElementById("user-input");// get the user's input
 
-add.addEventListener("click", function() {
+add.addEventListener("click", addToList);// add when clicked
+
+function addToList() {// add
     console.log(input.value);
-});
+}
+function removeFromList() {// remove
+    
+}
