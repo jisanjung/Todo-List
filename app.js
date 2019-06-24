d8f6bf131aab2ad3ee71afcb3c56cf2b674559dd
Jisan Jung <jjjung13@gmail.com>
2019-06-24 02:23:08 -0400

    got input to show on UI

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
+//--------------VARIABLES--------------------------------------------
 var add = document.getElementById("add-btn");// button to add to the list
 var input = document.getElementById("user-input");// get the user's input
+var listContainer = document.querySelector(".list-container");
 
+//--------------EVENT LISTENERS-------------------------------------
 add.addEventListener("click", addToList);// add when clicked
 
+//----------------FUNCTIONS-----------------------------------------
 function addToList() {// add
-    console.log(input.value);
+    
+    var listItem = document.createElement("li");// create an li element
+    listItem.classList.add("alert");// bootstrap classes
+    listItem.classList.add("alert-warning");
+    var userInput = document.createTextNode(input.value);
+    listItem.appendChild(userInput);// add the input into the li
+    
+    listContainer.appendChild(listItem);// add the li into ul
+    clearInput();
 }
 function removeFromList() {// remove
     
+}
+function clearInput() {// clears the input box
+    input.value = "";
 }
