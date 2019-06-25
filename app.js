60cf39a404715f8461f88a405a478b479493f3d8
Jisan Jung <jjjung13@gmail.com>
2019-06-25 14:48:23 -0400

    added condition when input value is not valid

--- app.js
+++ app.js
@@ -9,31 +9,35 @@ add.addEventListener("click", addToList);// add when clicked
 //----------------FUNCTIONS-----------------------------------------
 function addToList() {// add
     
-    //LIST ITEMS
-    var listItem = document.createElement("li");// create an li element
-    var close = document.createElement("img");// close icon
-    close.setAttribute("src", "images/cross.png");
-    listItem.classList.add("alert");
-    listItem.classList.add("alert-warning");
-    var userInput = document.createTextNode(input.value);
-    listItem.appendChild(userInput);// add the input into the li
-    listItem.appendChild(close);// add icon to li
-    
-    //APPEND LI TO UL AND CLEAR INPUT BOX
-    listContainer.appendChild(listItem);
-    clearInput();
-    
-    //CROSS OUT
-    listItem.addEventListener("click", function() {
-        listItem.style.textDecoration = "line-through";
-        listItem.classList.remove("alert-warning");
-        listItem.classList.add("alert-secondary");
-    });
-    
-    //REMOVE FROM LIST
-    close.addEventListener("click", function() {
-        listContainer.removeChild(listItem);
-    });
+    if (input.value === "" || input.value == null || input.value === undefined) {
+        alert("Nothing added to list");
+        } else {
+            //LIST ITEMS
+            var listItem = document.createElement("li");// create an li element
+            var close = document.createElement("img");// close icon
+            close.setAttribute("src", "images/cross.png");
+            listItem.classList.add("alert");
+            listItem.classList.add("alert-warning");
+            var userInput = document.createTextNode(input.value);
+            listItem.appendChild(userInput);// add the input into the li
+            listItem.appendChild(close);// add icon to li
+
+            //APPEND LI TO UL AND CLEAR INPUT BOX
+            listContainer.appendChild(listItem);
+            clearInput();
+
+            //CROSS OUT
+            listItem.addEventListener("click", function() {
+                listItem.style.textDecoration = "line-through";
+                listItem.classList.remove("alert-warning");
+                listItem.classList.add("alert-secondary");
+            });
+
+            //REMOVE FROM LIST
+            close.addEventListener("click", function() {
+                listContainer.removeChild(listItem);
+            });
+    }
 }
 function clearInput() {// clears the input box
     input.value = "";
