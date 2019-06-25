f2b6cf02283c8bf2a59b04f8e2a8317c47f0b5e2
Jisan Jung <jjjung13@gmail.com>
2019-06-25 14:38:23 -0400

    remove feature

--- app.js
+++ app.js
@@ -9,19 +9,30 @@ add.addEventListener("click", addToList);// add when clicked
 //----------------FUNCTIONS-----------------------------------------
 function addToList() {// add
     
+    //LIST ITEMS
     var listItem = document.createElement("li");// create an li element
+    var close = document.createElement("img");// close icon
+    close.setAttribute("src", "images/cross.png");
     listItem.classList.add("alert");
     listItem.classList.add("alert-warning");
     var userInput = document.createTextNode(input.value);
     listItem.appendChild(userInput);// add the input into the li
+    listItem.appendChild(close);// add icon to li
     
-    listContainer.appendChild(listItem);// add the li into ul
+    //APPEND LI TO UL AND CLEAR INPUT BOX
+    listContainer.appendChild(listItem);
     clearInput();
     
-    listItem.addEventListener("click", function() {// cross off list
+    //CROSS OUT
+    listItem.addEventListener("click", function() {
         listItem.style.textDecoration = "line-through";
         listItem.classList.remove("alert-warning");
         listItem.classList.add("alert-secondary");
+    });
+    
+    //REMOVE FROM LIST
+    close.addEventListener("click", function() {
+        listContainer.removeChild(listItem);
     });
 }
 function clearInput() {// clears the input box
